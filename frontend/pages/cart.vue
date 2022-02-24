013031ecf1c3bdb768ee51984bc41eb5ee18eb4c
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-02-24 15:51:36 +0700

    get data for table

--- frontend/pages/cart.vue
+++ frontend/pages/cart.vue
@@ -1,46 +1,55 @@
 <template>
-    <div>
-        <h1>page du panier</h1>
-        <p>{{cartArticles}}</p>
-        <p>{{listProducts}}</p>
-        <nuxt-link 
-            style="text-decoration: none;" 
-            :to="localePath('/')"
-          >
-            <v-btn>
-                back
-            </v-btn>
-          </nuxt-link>
-    </div>
+  <div>
+    <h1>page du panier</h1>
+    <p>{{ cartArticles }}</p>
+    <p>{{ table }}</p>
+    <v-btn @click="test">test</v-btn>
+    <nuxt-link style="text-decoration: none" :to="localePath('/')">
+      <v-btn> back </v-btn>
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                cartArticles: undefined,
-            }
-        },
-        props: {
-            serverDatas: {
-                type: Array,
-            },
-        },
-        computed: {
-            listProducts() {
-                return this.serverDatas
-            }
-        },
-        mounted() {
-            try{
-                this.cartArticles = JSON.parse(localStorage.getItem('Ecommerce')).cart   
-            } catch(e) {
-                console.log(e);
-            }
-        },
+export default {
+  data() {
+    return {
+      cartArticles: undefined,
+    };
+  },
+  props: {
+    serverDatas: {
+      type: Array,
+    },
+  },
+  computed: {
+    listProducts() {
+      return this.serverDatas;
+    },
+    table() {
+        this.cartArticles = JSON.parse(localStorage.getItem("Ecommerce"));
+        return this.cartArticles
     }
+  },
+  methods: {
+    test() {
+      const map1 = this.cartArticles.map((el) => {
+        let isInCart = (element) => element.id === el.product;
+        let index = this.listProducts.findIndex(isInCart);
+        console.log(index);
+        el.product = this.listProducts[index];
+      });
+    },
+  },
+  mounted() {
+    try {
+      this.cartArticles = JSON.parse(localStorage.getItem("Ecommerce"));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
